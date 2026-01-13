26d3bfbe824f50697d3adf8d87ef5d6347f485c0
Sampath Kumar <sampathk.hps18@gmail.com>
2026-01-13 08:59:32 +0530

    HF - config

---
+++ Learnings/FastAPIDemo/main.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
+
+from server.main import app
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=7860)
